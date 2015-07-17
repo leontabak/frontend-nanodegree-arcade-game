d31a45c8c7d43930c1aeb5d43097dfbf6a5ba19a
Leon Tabak <l.tabak@ieee.org>
2015-07-17 08:46:09 -0500

    Get background on screen

--- js/app.js
+++ js/app.js
@@ -2,11 +2,26 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    var obj = Object.create(Enemy.prototype);
+    this.x = 0;
+    this.y = 0;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-}
+    obj.sprite = 'images/enemy-bug.png';
+    return obj;
+};
+
+Enemy.prototype.whoami = function() {
+  console.log( "I am an enemy." );
+};
+
+Enemy.prototype.getimage = function() {
+    //return Resources.get(this.sprite);
+    return this.sprite;
+};
+
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -14,6 +29,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += 1;
+    this.y += 1;
 }
 
 // Draw the enemy on the screen, required method for game
@@ -24,11 +41,39 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    var obj = Object.create(Enemy.prototype);
+    this.x = 200;
+    this.y = 200;
+    this.sprite = 'images/char-boy.png';
+    return obj;
+} 
+
+Player.prototype.update = function() {
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function() {
+}
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
+var e = new Enemy();
+console.log( typeof e );
+allEnemies.push(e);
+var i;
+for( i = 0; i < allEnemies.length; i += 1 ) {
+    allEnemies[i].whoami();
+    var pic = allEnemies[i].getimage();
+    console.log( pic );
+} // for
+var player = new Player();
 
 
 
